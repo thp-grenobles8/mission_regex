073efcb3b8c1744a27d3292dc8eae82fb66dbdb2
gui3 <guillaume.3.7.13@gmail.com>
2019-04-06 22:10:22 +0200

    writing tuto

---
+++ js/display.js
@@ -0,0 +1,12 @@
+
+var d = {
+  toggleNav: function () {
+    let e=document.getElementById("mainNav")
+      .style;
+    if (e.display == "") {
+      e.display="block";
+    } else {
+      e.display="";
+    }
+  }
+}
